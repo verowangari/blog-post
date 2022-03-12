6d94a513875d313f33161f5e3098d774fa59c10a
verowangari <verowangari34@gmail.com>
2022-03-12 21:12:25 +0300

    create database

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+
+db=SQLAlchemy
+DB_NAME="database.db"
+
+
+
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='helloworld'
