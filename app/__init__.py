72b5bc4b9b450cb717ca0e227cecd65000a40c67
verowangari <verowangari34@gmail.com>
2022-03-12 21:14:28 +0300

    configure database

--- app/__init__.py
+++ app/__init__.py
@@ -11,6 +11,8 @@ DB_NAME="database.db"
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']='helloworld'
+    app.config['SQL_ALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    db.init_app(app)
     
     from .views import views
     from .auth import auth
