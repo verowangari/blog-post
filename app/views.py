2bd72ff3a56ed3ad458ff63f9360697416eca58c
verowangari <verowangari34@gmail.com>
2022-03-13 00:24:52 +0300

    import Post model and db in the views file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import  User,Post
 from . import db
 
 views = Blueprint("views", __name__)
