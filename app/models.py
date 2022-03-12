b772a21db01f57b33d422d74ad3cf21ebac76c04
verowangari <verowangari34@gmail.com>
2022-03-13 01:34:01 +0300

    create a likes relationship for post and user model

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
     posts = db.relationship('Post', backref='user', passive_deletes=True)
     comments = db.relationship('Comment', backref='user', passive_deletes=True)
+    likes = db.relationship('Like', backref='user', passive_deletes=True)
 
 
 class Post(db.Model):
@@ -20,6 +21,7 @@ class Post(db.Model):
     author = db.Column(db.Integer, db.ForeignKey(
         'user.id', ondelete="CASCADE"), nullable=False)
     comments = db.relationship('Comment', backref='post', passive_deletes=True)
+    likes = db.relationship('Like', backref='post', passive_deletes=True)
 
 
 class Comment(db.Model):
