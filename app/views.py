9b3dc685be02d0cff388e89b24b97107922d3f0e
verowangari <verowangari34@gmail.com>
2022-03-12 14:45:12 +0300

    route for different html files

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
-views =Blueprint('views',__name__)
+views = Blueprint("views", __name__)
 
-# @views.route('/')
-# def home():
-#     return "Home"
+
+@views.route("/")
+@views.route("/home")
+def home():
+    return render_template("home.html")
