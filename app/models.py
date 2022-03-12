2feced99e8c698cb8a7a368f373ef236083d2eab
verowangari <verowangari34@gmail.com>
2022-03-13 00:06:54 +0300

    add posts relationship in the user model

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    
+    posts = db.relationship('Post', backref='user', passive_deletes=True)   
     
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
